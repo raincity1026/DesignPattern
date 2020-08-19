fc59d83b7ff324b5d6a7e652cbdae30e57e68484
dnschen <dnschen@tencent.com>
2020-08-20 00:04:38 +0800

    abstract-factory sample code

---
+++ abstract-factory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  abstract-factory
+//
+//  Created by Dennis on 2020/8/19.
+//  Copyright © 2020 raincity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // same with factory method, the diffrence just one factory can be produce more than one product
+    }
+    return 0;
+}
